95d76fb4a0cc4b73be3840463708e77472445f73
Vid <savnikvid5@gmail.com>
2023-11-07 20:39:19 +0100

    monsters json

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
-import monstersData from '../../monsters.json';
+import monstersData from '../../mosnters_new.json';
 
 export default function Home() {
   const monsters = monstersData;
+  monsters.sort((a, b) => {
+      const nameA = a.type.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.type.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+          return -1;
+      }
+      if (nameA > nameB) {
+           return 1;
+      }
+
+        // names must be equal
+      return 0;
+    });
   return (
       <>
         {monsters.map((monster) => (
-            <div key={monster.code}>{monster.code}</div>
+            <div key={monster.name}>
+                <p>{monster.name}</p>
+                <p>{monster.type}</p>
+                <p>{monster.desc}</p>
+            </div>
+
         ))}
       </>
   )
